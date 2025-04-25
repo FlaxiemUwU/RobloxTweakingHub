db6102849cd489f199dbb64dc16ee6171640e913
FlaxiemUwU <destinxlie@gmail.com>
2025-04-25 02:33:56 -0400

    Update Unload.lua

--- Unload.lua
+++ Unload.lua
@@ -1,22 +1,9 @@
-print("Unloading...")
-print("-----")
 if smokeConnection then
-	print(">🔴smokeConnection Disconnected.")
 	smokeConnection:Disconnect()
 end
 if molotovConnection then
-	print(">🔴molotovConnection Disconnected.")
 	molotovConnection:Disconnect()
 end
-if smoke2Connection then
-	print(">🔴smoke2Connection Disconnected.")
-	smoke2Connection:Disconnect()
-end
-if molotov2Connection then
-	print(">🔴molotov2Connection Disconnected.")
-	molotov2Connection:Disconnect()
-end
-print("Unloaded")
 if Library then
 	Library:Unload()
 end
